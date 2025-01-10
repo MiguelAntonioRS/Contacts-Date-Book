755b2d47622bbb78a62b89303e230cce2af8a083
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-10 11:47:08 -0500

    first commit

---
+++ src/main/java/com/migueldev/contactsDateBook/ContactsDateBookApplication.java
@@ -0,0 +1,13 @@
+package com.migueldev.contactsDateBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsDateBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsDateBookApplication.class, args);
+	}
+
+}

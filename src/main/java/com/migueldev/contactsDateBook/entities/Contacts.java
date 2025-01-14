3203cfa7e08e72d008eec5df61e4d50d04c3619b
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-14 11:03:00 -0500

    Creando el id

--- src/main/java/com/migueldev/contactsDateBook/entities/Contacts.java
+++ src/main/java/com/migueldev/contactsDateBook/entities/Contacts.java
@@ -1,4 +1,14 @@
 package com.migueldev.contactsDateBook.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Contacts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }

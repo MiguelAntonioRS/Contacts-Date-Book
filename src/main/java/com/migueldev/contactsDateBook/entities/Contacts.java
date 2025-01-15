6c9ad75c3b2723ecb1aa5bc1f72b1a1ba166266a
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-14 22:15:30 -0500

    Agregando todos los campos que conlleva la entidad Contacts

--- src/main/java/com/migueldev/contactsDateBook/entities/Contacts.java
+++ src/main/java/com/migueldev/contactsDateBook/entities/Contacts.java
@@ -5,10 +5,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Entity
 public class Contacts {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private LocalDate bornDate;
+
+    private LocalDateTime registryDate;
 }

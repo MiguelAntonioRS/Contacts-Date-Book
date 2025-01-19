75fcb75dcfe7c493b6999517f7de31b10c640489
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-19 18:29:26 -0500

    Ya es funcionable el inicio de la app

--- src/main/java/com/migueldev/contactsDateBook/controller/ContactsController.java
+++ src/main/java/com/migueldev/contactsDateBook/controller/ContactsController.java
@@ -1,6 +1,8 @@
 package com.migueldev.contactsDateBook.controller;
 
 import com.migueldev.contactsDateBook.entities.Contacts;
+import com.migueldev.contactsDateBook.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class ContactsController {
 
+    @Autowired
+    private ContactService contactService;
 
     @GetMapping({"/",""})
     public String homePage() {
@@ -24,6 +28,8 @@ public class ContactsController {
 
     @PostMapping("/new")
     public String saveContact(Contacts contacts, RedirectAttributes redirect) {
+        contactService.save(contacts);
+        redirect.addFlashAttribute("msgSucc", "El contacto se guardo satisfactoriamente");
         return "redirect:/";
     }
 }

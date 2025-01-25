e94041880632d822b27dc1dad772ba51b691ed4f
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-25 18:34:09 -0500

    Validanto las variables usando la libreria validation de spring

--- src/main/java/com/migueldev/contactsDateBook/entities/Contacts.java
+++ src/main/java/com/migueldev/contactsDateBook/entities/Contacts.java
@@ -1,10 +1,16 @@
 package com.migueldev.contactsDateBook.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -19,12 +25,18 @@ public class Contacts {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank(message = "Campo vacio: Ingrese su nombre")
     private String name;
 
+    @NotEmpty(message = "Campo vacio: Ingrese su email")
     private String email;
 
+    @NotBlank(message = "Campo vacio: Ingrese su # de celular")
     private String phone;
 
+    @DateTimeFormat(iso = ISO.DATE)
+    @Past
+    @NotNull(message = "Ingrese correctamente su fecha de nacimiento")
     private LocalDate bornDate;
 
     private LocalDateTime registryDate;
